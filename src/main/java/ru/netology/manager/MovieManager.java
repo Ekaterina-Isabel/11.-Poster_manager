1300284083e7eab4ba5a76b69a6cb210a2ab5758
kate <vuk_kat@mail.ru>
2022-03-13 15:05:05 +0500

    add MovieManager

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,5 +1,71 @@
 package ru.netology.manager;
 
-public class MovieManager {
+import ru.netology.domain.Movie;
 
+public class MovieManager {
+    private Movie[] movies = new Movie[0];      //movies - поле, в котором создан пустой массив
+
+    private int resultLength;
+
+    public int getResultLength() {
+        return resultLength;
+    }
+
+    public Movie[] getMovies() {
+        return movies;
+    }
+
+    public MovieManager() {
+        resultLength = 10;
+    }
+
+    public MovieManager(int resultLength) {
+        if (resultLength <= 0) {
+            resultLength = 10;
+        } else {
+            this.resultLength = resultLength;
+        }
+    }
+
+    public void addNewMovie(Movie movie) {      //добавление нового фильма, принимает и сохраняет один элемент movie
+        int length = movies.length + 1;     //вычисление длинны нового массива = длинна старого массива + 1
+        Movie[] tmp = new Movie[length];        //создание нового массива из фильмов
+        System.arraycopy(movies, 0, tmp, 0, movies.length);     //метод для автоматического копирования элементов из старого массива в новый
+        int lastIndex = tmp.length - 1;     //вычисляем номер последней ячейки
+        tmp[lastIndex] = movie;     //добавляем в нее новый фильм
+        movies = tmp;       //сохраняем новый массив в поле movies
+    }
+
+    public Movie[] findAll() {      //вывод всех фильмов в порядке добавления
+        Movie[] result = new Movie[movies.length];
+        for (int i = 0; i < result.length; i++) {       // перебираем массив в прямом порядке, но кладем в результаты в обратном
+            result[i] = movies[i];
+        }
+        return result;
+    }
+
+    private Movie[] revertMovies() {      //вывод всех фильмов в обратном от добавления порядке
+        Movie[] result = new Movie[movies.length];
+        for (int i = 0; i < result.length; i++) {       //перебираем массив в прямом порядке, но кладем в результаты в обратном
+            int movieIndex = movies.length - i - 1;
+            result[i] = movies[movieIndex];
+        }
+        return result;
+    }
+
+    public Movie[] findLast() {
+        int calculatedResultLength;
+        if (movies.length < resultLength) {
+            calculatedResultLength = movies.length;
+        } else {
+            calculatedResultLength = resultLength;
+        }
+
+        Movie[] result = new Movie[calculatedResultLength];
+        Movie[] allMovie = revertMovies();
+        for (int i = 0; i < calculatedResultLength; i++) {
+            result[i] = allMovie[i];
+        }
+        return result;
+    }
 }
